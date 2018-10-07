aaaa0aaeccf89facb6a2b3bfa78568d8ae57ff56
Aestek <thib.gilles@gmail.com>
2018-10-07 21:16:06 +0200

    Colors colors

--- cpu.go
+++ cpu.go
@@ -14,7 +14,12 @@ func CPU(interval time.Duration) Slot {
 			Icon("\uf0e4", ColorHighlight3),
 			Style(
 				fmt.Sprintf(" %2.0f%%", p[0]),
-				Grad(p[0], GradStop{0, ColorText}, GradStop{50, ColorWarning}, GradStop{100, ColorDanger}),
+				Grad(p[0],
+					GradStop{0, ColorText},
+					GradStop{25, ColorText},
+					GradStop{50, ColorWarning},
+					GradStop{100, ColorDanger},
+				),
 				FontMono,
 			),
 		)
