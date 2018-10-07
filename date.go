a39077e655ecdb64ddf1a5263bcd7d525a466f50
Aestek <thib.gilles@gmail.com>
2018-10-07 18:43:58 +0200

    Re-add calendar

--- date.go
+++ date.go
@@ -9,7 +9,10 @@ func Date() Slot {
 
 	go func() {
 		for {
-			s <- time.Now().Format("Mon, _2 Jan 15:04")
+			s <- Comb(
+				Icon("\uf073  ", ColorHighlight2),
+				time.Now().Format("Mon, _2 Jan 15:04"),
+			)
 			now := time.Now()
 			time.Sleep(time.Minute - time.Second*time.Duration(now.Second()) + time.Second - time.Nanosecond*time.Duration(now.Nanosecond()))
 		}
