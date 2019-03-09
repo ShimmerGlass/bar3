edd3ccd80c8e9098c4715f0e335578b05f7528de
Aestek <thib.gilles@gmail.com>
2019-03-09 11:04:57 +0100

    Fork to log crashes

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"log"
 	"log/syslog"
 	"os"
+	"os/exec"
 	"time"
 )
 
@@ -16,6 +18,33 @@ func main() {
 	if err == nil {
 		log.SetOutput(logwriter)
 	}
+
+	if _, ok := os.LookupEnv("BAR3_FORK"); !ok {
+		log.Println("Startup: forking")
+
+		cmd := exec.Command(os.Args[0], os.Args[1:]...)
+		cmd.Env = append(os.Environ(), "BAR3_FORK=1")
+		cmd.Stdout = os.Stdout
+
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			panic(err)
+		}
+		go func() {
+			r := bufio.NewScanner(stderr)
+			for r.Scan() {
+				log.Println("stderr: ", r.Text())
+			}
+		}()
+
+		err = cmd.Run()
+		if err != nil {
+			log.Println("fork exited with error: ", err)
+		}
+		return
+	}
+
+	log.Println("Startup: fork")
 
 	w := NewWriter(os.Stdout)
 	Run(w, Style("  ╱  ", ColorInactive),
